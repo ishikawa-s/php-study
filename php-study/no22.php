76dcbd264828ed66e19902f2478885662e1e4095
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-05-08 17:17:25 +0900

    update static property

--- php-study/no22.php
+++ php-study/no22.php
@@ -4,8 +4,10 @@
 
 class User{
 	public $name;
+	public static $count = 0;
 	public function __construct($name){
 		$this->name = $name;
+		self::$count++;
 	}
 	public function sayHi(){
 		echo "hi, i am $this->name!";
@@ -15,6 +17,9 @@ class User{
 	}
 }
 
-User::getMessage();
+//User::getMessage();
 
-//$tom = new User("Tom");
+$tom = new User("Tom");
+$bob = new User("Bob");
+
+echo User::$count;
