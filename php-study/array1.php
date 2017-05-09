95f11e7cda260a21bf179fa0e2180031576624b1
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-05-09 12:05:59 +0900

    PHPテキスト no6

---
+++ php-study/array1.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-type: text/html; charset=UTF-8');
+?>
+
+<html>
+<body>
+<?php
+	$arr1[] = '123';
+	$arr1[] = '季節';
+	$arr1[] = 456.789;
+
+	$arr2['spring'] = '春';
+	$arr2['summer'] = '夏';
+	$arr2['autumn'] = '秋';
+	$arr2['winter'] = '冬';
+
+	echo $arr1[0], '<br>';
+	echo $arr1[1], '<br>';
+	echo $arr1[2], '<br>';
+
+	echo $arr2['spring'], '<br>';
+	echo $arr2['summer'], '<br>';
+	echo $arr2['autumn'], '<br>';
+	echo $arr2['winter'], '<br>';
+
+	?>
+</body>
+</html>
