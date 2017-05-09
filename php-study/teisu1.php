55ab61c083a1a2f149666530dcb7b91c8fec0765
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-05-09 11:42:34 +0900

    PHPテキスト no5

---
+++ php-study/teisu1.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-type: text/html; charset=UTF-8');
+?>
+
+<html>
+<head>
+<title>
+<?php
+	define('NAME_THEME', '盆栽');
+	echo NAME_THEME, 'クラブ';
+?>
+</title>
+</head>
+<body>
+<?php
+	echo NAME_THEME, 'について';
+?>
+</body>
+</html>
