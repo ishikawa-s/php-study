fac59e777d60a4fb807329f4cb69f9ec04e680c0
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-05-09 11:49:34 +0900

    PHPテキスト no5

---
+++ php-study/teisu2.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-type: text/html; charset=UTF-8');
+?>
+
+<html>
+<body>
+<?php
+	echo '始まり<br>';
+	echo __LINE__, '行目です<br>';
+	echo '途中<br>';
+	echo __LINE__, '行目です<br>';
+	echo '終わり<br>';
+?>
+</body>
+</html>
