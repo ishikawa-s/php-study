605b9b13334dd980e9911a3e623093cc3eac388d
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-05-09 15:04:23 +0900

    PHPテキスト no18?

---
+++ php-study/switch1.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-type:text/html; charset=UTF-8');
+?>
+
+<html>
+<body>
+<?php
+	$str='猫';
+	echo $str, 'です。<br>', PHP_EOL;
+
+	switch ($str){
+		case '犬':
+			echo 'ワンと鳴きます。';
+			break;
+		case '猫';
+			echo 'ニャーと鳴きます。';
+			break;
+		case '牛';
+			echo 'モーと鳴きます。';
+			break;
+		default:
+			echo 'どのように鳴くのだろう？';
+
+	}
+?>
+</body>
+</html>
