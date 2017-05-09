0615f76a7a5d115e87eadc4b208af8f485d8a1d9
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-05-09 14:41:26 +0900

    PHPテキスト no16

---
+++ php-study/if2.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-type: text/html; charset=UTF-8');
+?>
+
+<html>
+<body>
+<?php
+	$rateRt = 45;
+	echo '降水確率は', $rateRt, '%です。<br>';
+
+	if ($rateRt >= 70) {
+		echo 'long umbrella !!';
+	}else if($rateRt >= 40){
+		echo 'short umbrella !!';
+	}else{
+		echo 'nothing !!';
+	}
+?>
+</body>
+</html>
