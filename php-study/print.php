58ed21f3d588660fda47652093099fdde45689c1
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-05-09 10:50:30 +0900

    PHPテキスト no2

---
+++ php-study/print.php
@@ -0,0 +1,13 @@
+<?php
+	header('Content-type: text/html; charset=UTF8');
+?>
+
+<html>
+<body>
+<?php
+	echo "みなさん、" , "おはよう。<br>", PHP_EOL;
+	print "朝が早い" . "とても眠い<br><br>" . PHP_EOL;
+	echo "がんばりましょう。いえあ。<br>", PHP_EOL;
+?>
+</body>
+</html>
