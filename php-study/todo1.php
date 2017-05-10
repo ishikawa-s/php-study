a765326b79f8f1f8aaa2433c19dfab18d6157b70
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-05-10 12:56:25 +0900

    create todo

---
+++ php-study/todo1.php
@@ -0,0 +1,52 @@
+<?php header('Contenttype:text/html; charset=UTF-8'); ?>
+
+<html>
+<body>
+	<form method='post' action="todo1.php">
+		<input type="text" name="todocont" size="30" maxlength="100">
+	<?php
+	$prioDisp = 2;
+	$selStr = array('', '', '');
+	$prio = 0;
+	if(isset($_POST['priority'])){
+		$prio = (int)$_POST['priority'];
+	}
+	?>
+		<select name='priority'>
+		<option value="2">すべて
+		<option value="1">高
+		<option value="0">低
+		</select>
+		<br><br>
+		<input type="submit" name="insert" value="追加">
+		<input type="submit" name="search" value="検索">
+		<input type="submit" name="delete" value="削除">
+	<pre>
+	<?php
+		$dsn = 'mysql:dbname=php_test; host=localhost; charset=utf-8';
+		$user = 'ishikawa_s';
+		$password = 'ishikawa_s';
+		try{
+			$dbh = new PDO($dsn, $user, $password);
+		}catch (PDOException $e){
+			die('Connect error: '. $e->getCode());
+		}
+
+		$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+		if(isset($_POST['insert'], $_POST['todocont']) && $_POST['todocont']!=''){
+			if($prio < 0 || $prio > 1){
+				$prio = 0;
+			}
+			$sql = 'INSERT INTO todolist (todo, prio, created) VALUES (?, ?, CURDATE())';
+			$sth = $dbh->prepare($sql);
+			$sth->bindValue(1, $_POST['todocont'], PDO::PARAM_STR);
+			$sth->bindValue(2, $prio, PDO::PARAM_INT);
+			$sth->execute();
+		}
+		$dbh = null;
+	?>
+	</pre>
+	</form>
+</body>
+</html>
